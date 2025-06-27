46bf11114ea2e944d560cb34acb5e4a19a93acf1
xuxj <760409558@qq.com>
2025-06-27 17:20:24 +0800

    feat: load dataset

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import os
 import csv
-from transformers import BertTokenizer, AutoTokenizer, PreTrainedTokenizer
+from transformers import AutoTokenizer
 
 DATA_DIR = './data'
 MODEL_PATH = "D:\\models\\chinese-bert-wwm"
@@ -9,31 +9,54 @@ train_file = os.path.join(DATA_DIR, 'train.csv')
 valid_file = os.path.join(DATA_DIR, 'valid.csv')
 test_file = os.path.join(DATA_DIR, 'test.csv')
 
+def load_relations():
+    with open(os.path.join(DATA_DIR, 'relation.csv'), 'r', encoding='utf-8') as f:
+        reader = csv.reader(f.readlines())
+    
+    rows = [row for row in reader][1:]
+    relations = {}
+    for row in rows:
+        relations[row[2]] = {
+            'head_type': row[0],
+            'tail_type': row[1],
+            'index': int(row[3])
+        }
+    return relations
+
+
 with open(train_file, 'r', encoding='utf-8') as f:
     reader = csv.reader(f.readlines())
 
-def preprocess_data(data_rows: list[list[str]], tokenizer: AutoTokenizer):
+
+def preprocess_data(data_rows: list[list[str]], tokenizer: AutoTokenizer, relations: dict):
     data = []
-    for row in data_rows[6:]:
+    for row in data_rows:
         [sentence, rel, e1, e1_off, e2, e2_off] = row
-        e1, e2 = row[3], row[5]
         inputs = tokenizer(sentence, return_offsets_mapping=True)
         e1_mask = [0] * len(inputs['input_ids'])
         e2_mask = [0] * len(inputs['input_ids'])
-        tokens = tokenizer.convert_ids_to_tokens(inputs['input_ids'])
-        print(tokens)
         e1_off, e2_off = int(e1_off), int(e2_off)
-        e1_len, e2_len = len(e1), len(e2)
+        idx = 0
         for m in inputs['offset_mapping']:
             (start, end) = m
             if start == 0 and end == 0:
+                idx += 1
                 continue
-            if (start < e1_off or start > e1_off+len(e1)) and (start < e2_off or start > e2_off+len(e2)):
-                continue
-            print(sentence[start:end+1])
-        break
-    pass
+            if (start >= e1_off and start < e1_off + len(e1)):
+                e1_mask[idx] = 1
+            if (start >= e2_off and start < e2_off + len(e2)):
+                e2_mask[idx] = 1
+            idx += 1
+        data.append({
+            'input_ids': inputs['input_ids'],
+            'attention_mask': inputs['attention_mask'],
+            'e1_mask': e1_mask,
+            'e2_mask': e2_mask,
+            'label': relations.get(rel)['index'],
+        })
+    return data
 
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-preprocess_data([row for row in reader], tokenizer)
+relations = load_relations()
+preprocess_data([row for row in reader][1:], tokenizer, relations)
